eb520f80d29ca0c889eab805d17e96080439fd1b
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-05 15:40:12 +0200

    Initial commit with project skeleton

---
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/ZadanieRekrutacyjneClApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.zadanie_rekrutacyjne_cl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieRekrutacyjneClApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieRekrutacyjneClApplication.class, args);
+    }
+
+}
