bfc8d9c0c9cda16d266e2250fc51741cb8d226ef
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-05 16:30:02 +0200

    Created AnswerServiceInterface and its implementation AnswerService

---
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/service/AnswerService.java
@@ -0,0 +1,31 @@
+package pl.coderslab.zadanie_rekrutacyjne_cl.service;
+
+import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Answer;
+import pl.coderslab.zadanie_rekrutacyjne_cl.repository.AnswerRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AnswerService implements AnswerServiceInterface {
+
+    private final AnswerRepository answerRepository;
+
+    public AnswerService(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    @Override
+    public void save(Answer answer) {
+        answerRepository.save(answer);
+    }
+
+    @Override
+    public Optional<Answer> findById(Long id) {
+        return answerRepository.findById(id);
+    }
+
+    @Override
+    public List<Answer> findAll() {
+        return answerRepository.findAll();
+    }
+}
---
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/service/AnswerServiceInterface.java
@@ -0,0 +1,12 @@
+package pl.coderslab.zadanie_rekrutacyjne_cl.service;
+
+import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Answer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AnswerServiceInterface {
+    void save(Answer answer);
+    Optional<Answer> findById(Long id);
+    List<Answer> findAll();
+}
