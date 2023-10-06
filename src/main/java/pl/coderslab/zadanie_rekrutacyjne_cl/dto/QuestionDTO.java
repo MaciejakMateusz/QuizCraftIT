97ea6239e2b67bf0e4f6292780f9e45223e5a510
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-06 14:32:15 +0200

    Expanded QuestionDTO, making it ready to be used as a vessel for API data

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/dto/QuestionDTO.java
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/dto/QuestionDTO.java
@@ -2,10 +2,22 @@ package pl.coderslab.zadanie_rekrutacyjne_cl.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Map;
 
 @Getter
 @Setter
+@ToString
 public class QuestionDTO {
-    private long id, api_id;
+    private int id;
     private String question;
+    private String description;
+    private Map<String, String> answers;
+    private boolean multipleCorrectAnswers;
+    private Map<String, Boolean> correctAnswers;
+    private String explanation;
+    private String tip;
+    private String category;
+    private String difficulty;
 }
