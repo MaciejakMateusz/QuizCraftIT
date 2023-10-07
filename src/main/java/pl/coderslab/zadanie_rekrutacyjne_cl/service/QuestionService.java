af03d0be8cfdd9d2c80fb296ae97909d41ce091f
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-07 12:57:21 +0200

    Updated findRandom() method in QuestionService

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/service/QuestionService.java
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/service/QuestionService.java
@@ -1,19 +1,24 @@
 package pl.coderslab.zadanie_rekrutacyjne_cl.service;
 
 import org.springframework.stereotype.Service;
+import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Answer;
 import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Question;
+import pl.coderslab.zadanie_rekrutacyjne_cl.exception.CustomException;
 import pl.coderslab.zadanie_rekrutacyjne_cl.repository.QuestionRepository;
 import pl.coderslab.zadanie_rekrutacyjne_cl.service.interfaces.QuestionServiceInterface;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class QuestionService implements QuestionServiceInterface {
 
     private final QuestionRepository questionRepository;
+    private final AnswerService answerService;
 
-    public QuestionService(QuestionRepository questionRepository) {
+    public QuestionService(QuestionRepository questionRepository, AnswerService answerService) {
         this.questionRepository = questionRepository;
+        this.answerService = answerService;
     }
 
     @Override
@@ -23,6 +28,16 @@ public class QuestionService implements QuestionServiceInterface {
 
     @Override
     public Optional<Question> findRandom() {
-        return questionRepository.findRandomQuestion();
+
+        List<Answer> answers;
+        Optional<Question> randomQuestion = questionRepository.findRandomQuestion();
+
+        if (randomQuestion.isPresent()) {
+            answers = answerService.findAllByQuestion(randomQuestion
+                    .orElseThrow(() -> new CustomException("No answers found for this question")));
+            randomQuestion.get().setAnswers(answers);
+        }
+
+        return randomQuestion;
     }
 }
