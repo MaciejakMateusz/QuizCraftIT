7a7ece63a26db727c95f5f2451d3f450f855f34b
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-07 12:09:05 +0200

    Renamed ApiCommunicationException to CustomException, making it more usable in other parts of application

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/exception/ApiCommunicationException.java
+++
@@ -1,7 +0,0 @@
-package pl.coderslab.zadanie_rekrutacyjne_cl.exception;
-
-public class ApiCommunicationException extends RuntimeException{
-    public ApiCommunicationException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/exception/CustomException.java
@@ -0,0 +1,7 @@
+package pl.coderslab.zadanie_rekrutacyjne_cl.exception;
+
+public class CustomException extends RuntimeException{
+    public CustomException(String message) {
+        super(message);
+    }
+}
