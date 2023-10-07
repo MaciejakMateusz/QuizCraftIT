9c856946a9f84e3b97beeec846a4874adc8866c3
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-07 12:17:13 +0200

    - injected QuestionService and AnswerService in MainApiController - updated /questions endpoint to return random question or throw CustomException if there was a problem finding it

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/controller/rest/MainApiController.java
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/controller/rest/MainApiController.java
@@ -4,13 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Question;
+import pl.coderslab.zadanie_rekrutacyjne_cl.exception.CustomException;
+import pl.coderslab.zadanie_rekrutacyjne_cl.service.AnswerService;
+import pl.coderslab.zadanie_rekrutacyjne_cl.service.QuestionService;
 
 @RestController
 @RequestMapping("/api")
 public class MainApiController {
 
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    public MainApiController(QuestionService questionService, AnswerService answerService) {
+        this.questionService = questionService;
+        this.answerService = answerService;
+    }
+
     @GetMapping("/questions")
     public Question randomQuestion() {
-        return new Question();
+        return questionService.findRandom()
+                .orElseThrow(() -> new CustomException("Couldn't find a question, try again"));
     }
 }
