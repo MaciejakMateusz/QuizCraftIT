58d347c9d2cae1f0c8a400dacdbcc7fa0e3248aa
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-07 18:06:26 +0200

    Renamed QuestionAnswerRequestBody to AnswerRequestBody

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/dto/AnswerRequestBody.java
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/dto/AnswerRequestBody.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
-public class QuestionAnswerRequestBody {
+public class AnswerRequestBody {
     @JsonProperty("answers")
     private List<Long> answers;
 
