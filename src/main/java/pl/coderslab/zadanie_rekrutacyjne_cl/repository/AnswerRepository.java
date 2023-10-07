29f350a75c598499a93bec15f1c3aa308191c831
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-07 12:54:58 +0200

    Expanded AnswerRepository by findAllByQuestion() method

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/repository/AnswerRepository.java
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/repository/AnswerRepository.java
@@ -2,6 +2,10 @@ package pl.coderslab.zadanie_rekrutacyjne_cl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Answer;
+import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Question;
+
+import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findAllByQuestion(Question question);
 }
