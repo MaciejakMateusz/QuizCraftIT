5f3106f910c3a9a7d0ed0c4b31367a6981911e30
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-07 13:11:56 +0200

    - replaced CustomException with QuestionNotFoundException - added options request mapping

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/controller/rest/MainApiController.java
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/controller/rest/MainApiController.java
@@ -1,10 +1,14 @@
 package pl.coderslab.zadanie_rekrutacyjne_cl.controller.rest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Question;
-import pl.coderslab.zadanie_rekrutacyjne_cl.exception.CustomException;
+import pl.coderslab.zadanie_rekrutacyjne_cl.exception.QuestionNotFoundException;
 import pl.coderslab.zadanie_rekrutacyjne_cl.service.AnswerService;
 import pl.coderslab.zadanie_rekrutacyjne_cl.service.QuestionService;
 
@@ -23,6 +27,13 @@ public class MainApiController {
     @GetMapping("/questions")
     public Question randomQuestion() {
         return questionService.findRandom()
-                .orElseThrow(() -> new CustomException("Couldn't find a question, try again"));
+                .orElseThrow(() -> new QuestionNotFoundException("No question found, try again."));
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> options() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Allow", "GET, POST, OPTIONS");
+        return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 }
