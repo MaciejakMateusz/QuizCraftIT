6fb633cb0d71ab90015ba6f9a6e8d92ed59624a3
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-07 12:13:01 +0200

    Created new query in QuestionRepository to find one random Optional<Question> in database

--- src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/repository/QuestionRepository.java
+++ src/main/java/pl/coderslab/zadanie_rekrutacyjne_cl/repository/QuestionRepository.java
@@ -1,7 +1,13 @@
 package pl.coderslab.zadanie_rekrutacyjne_cl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.zadanie_rekrutacyjne_cl.entity.Question;
 
+import java.util.Optional;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    @Query(value = "SELECT q FROM Question q ORDER BY RAND() LIMIT 1")
+    Optional<Question> findRandomQuestion();
 }
