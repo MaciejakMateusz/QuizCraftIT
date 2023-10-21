ebe716f9c2b3e3f1bc5a3bee46a885f16c42548e
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-21 17:23:50 +0200

    Renamed ZadanieRekrutacyjneClApplication to QuizCraftApplication

--- src/main/java/pl/coderslab/quizcraft/QuizCraftApplication.java
+++ src/main/java/pl/coderslab/quizcraft/QuizCraftApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZadanieRekrutacyjneClApplication {
+public class QuizCraftApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZadanieRekrutacyjneClApplication.class, args);
+        SpringApplication.run(QuizCraftApplication.class, args);
     }
 
 }
--- src/main/java/pl/coderslab/quizcraft/ServletInitializer.java
+++ src/main/java/pl/coderslab/quizcraft/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ZadanieRekrutacyjneClApplication.class);
+        return application.sources(QuizCraftApplication.class);
     }
 
 }
